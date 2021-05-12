71f0a476fcc0117d7b8d79c486675c3d671e73f4
NIcolas Buitrago <nicolasbuitragobautista@gmail.com>
2021-05-12 15:41:19 -0500

    function to write message into csv added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from os import EX_TEMPFAIL
 from flask import Flask,render_template, request, redirect
-
+import csv
 app = Flask(__name__)
 
 
@@ -12,19 +12,20 @@ def home():
 def page(page_name):
     return render_template(page_name)
 
-@app.route('/submit_form',methods = ['POST','GET'])
+@app.route('/submit_form',methods = ['POST'])
 def submit_form():
     if request.method == 'POST':
-        data = request.form.to_dict()
-        write_to_file(data)
+        data =request.form.to_dict()
+        write_csv(data)
         return redirect('thankyou.html')
     else:
         return  redirect('contact.html')
 
-def write_to_file(data):
-    with open('messages.txt',mode = 'a') as msg:
+def write_csv(data):
+    with open('messages.csv',mode = 'a') as msg:
         email = data['email']
-        first_name = data['name']
         last_name = data['last name']
+        first_name = data['name']
         subject = data['message']
-        file = msg.write(f"\n{email},{first_name},{last_name},{last_name},{subject}")
+        csv_write = csv.writer(msg,delimiter = ',',quotechar = "'",quoting = csv.QUOTE_MINIMAL)
+        csv_write.writerow([email,first_name,last_name,subject])
